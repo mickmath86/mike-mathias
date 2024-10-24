c597ed3f787ee4380a791e67cb9bf0bd911ef4b5
Michael Mathias <mickmath86@gmail.com>
2024-10-24 11:35:08 -0700

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { type Metadata } from 'next'
+import { Analytics } from "@vercel/analytics/react"
 
 import { Providers } from '@/app/providers'
 import { Layout } from '@/components/Layout'
